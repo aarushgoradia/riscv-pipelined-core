4ad74a6253f308c40915b59cf0e2b466e10d5191
Aarush Goradia <goradia.aarush@gmail.com>
2025-07-15 11:44:45 +0800

    Finished main controller in decode

--- src/main_control.sv
+++ src/main_control.sv
@@ -1,3 +1,5 @@
+`timescale 1ns / 1ps
+
 module main_control (
     input logic [6:0] opcode,
     input logic [2:0] funct3,
@@ -11,6 +13,14 @@ module main_control (
     output logic alu_src,
     output logic [2:0] imm_sel
 );
+
+    typedef enum logic [2:0] {
+        IMM_I = 3'd0,
+        IMM_S = 3'd1,
+        IMM_B = 3'd2,
+        IMM_U = 3'd3,
+        IMM_J = 3'd4
+        } imm_sel_e;
 
     always_comb begin
         // defaults
@@ -55,4 +65,29 @@ module main_control (
                 alu_op = 2'b10;
                 alu_src = 1'b0;
             end
-            // Keep adding cases for other opcodes.
+            7'b1101111: begin // JAL
+                reg_write = 1'b1;
+                alu_op = 2'b00;
+                imm_sel = IMM_J;
+            end
+            7'b1100111: begin // JALR
+                reg_write = 1'b1;
+                alu_op = 2'b00;
+                alu_src = 1'b1;
+                imm_sel = IMM_I;
+            end
+            7'b0110111: begin // LUI
+                reg_write = 1'b1;
+                alu_src = 1'b1;
+                imm_sel = IMM_U;
+            end
+            7'b0010111: begin // AUIPC
+                reg_write = 1'b1;
+                alu_op = 2'b00;
+                alu_src = 1'b1;
+                imm_sel = IMM_U;
+            end
+            default: ;
+        endcase
+    end
+endmodule
